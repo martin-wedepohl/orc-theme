dd08497951fed39c4c3b098f728df2f1852ae3d4
Martin Wedepohl <martin@orchardrecovery.com>
2018-12-26 15:54:02 -0800

    1.1.7 Moved JS code to display titles under Post Grid from a plug in to the general javascript for the theme

--- js/general.js
+++ js/general.js
@@ -3,11 +3,19 @@
 	
 	jQuery(document).ready(function() {
 
+        // Open privacy page in a new window
 		$('a.privacy-policy-link').click(function(e) {
 			e.preventDefault();
 			var url = $(this).attr('href');
 			window.open(url, '_blank');
 		});
+        
+        // Display title of image below image in Post Grid
+        $('a.vc_gitem-link.vc-zone-link').each(function() {
+            var thetitle = $(this).attr('title');
+            $(this).parents('.vc_grid-item').append('<figure><figcaption class="aligncenter">' + thetitle + '</figcaption></figure>');
+        });
+        
 
    }); // End (document).ready
 	
